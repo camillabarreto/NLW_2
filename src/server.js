e8c48379f2acd38dfc3c92a5afa36ea016ab29b0
camillabarreto <camillabarretodesousa@gmail.com>
2020-08-06 21:19:22 -0300

    dinamismo do teacher-item com {%for%} no study.html

--- src/server.js
+++ src/server.js
@@ -1,16 +1,66 @@
-const express = require('express')
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "48988887766",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20,00",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "48988887766",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "40,00",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Camilla Barreto de Sousa",
+        avatar: "https://avatars0.githubusercontent.com/u/42206341?s=460&u=0f73dfc8431d8dabe79dcd381f844f6f8886aeb3&v=4",
+        whatsapp: "48988887766",
+        bio: "Atualmente to valendo nada.",
+        subject: "Nada",
+        cost: "0,00",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html", { proffys })
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
+const express = require("express")
 const server = express()
 
-server.use(express.static("public"))
+//configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+})
 
-    .get("/", (req, res) => { /* => isso é uma arrow function*/
-        return res.sendFile(__dirname + "/views/index.html")
-    })
-    .get("/study", (req, res) => {
-        return res.sendFile(__dirname + "/views/study.html")
-    })
-    .get("/give-classes", (req, res) => {
-        return res.sendFile(__dirname + "/views/give-classes.html")
-    })
-
+server
+    //configurar arquivos estaticos (css, scripts, images)
+    .use(express.static("public"))
+    //rotas da aplicação
+    .get("/", pageLanding)
+    .get("/study", pageStudy)
+    .get("/give-classes", pageGiveClasses)
     .listen(5500)
